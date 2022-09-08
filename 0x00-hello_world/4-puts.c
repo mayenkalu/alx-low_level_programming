cc4b371751d15a277b69b7808197b29a94becaaf
Mayen Kalu <kalumayen@gmail.com>
2022-09-08 16:27:02 -0700

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - "Programming is like building a multilingual puzzle
+ * main - print the string in the puts function
  *
  * return: Always 0 (success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle"\")
+	puts("\"Programming is like building a multilingual puzzle")
+
 	return (0);
 }
