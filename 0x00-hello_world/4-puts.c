f6ea3bfcc6dcd775267dd1893774f58cb2836041
Mayen Kalu <kalumayen@gmail.com>
2022-09-08 16:04:29 -0700

    added puts()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-	/**
-	 *main -  "Programming is like building a multilingual puzzle
-	 *	 *Return: 0 (success)
-	 */
+/**
+ * main - "Programming is like building a multilingual puzzle
+ *
+ * return: Always 0 (success)
+ */
 int main(void)
 {
+	puts("Programming is like building a multilingual puzzle"\")
 	return (0);
 }
