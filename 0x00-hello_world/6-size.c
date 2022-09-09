1b2e749ae2711949490c5720eab52ec8c47decc9
Mayen Kalu <kalumayen@gmail.com>
2022-09-08 20:57:05 -0700

    correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,20 +2,16 @@
 
 /**
  * main - prints the various strings with printf function
+ * 
  * Return: Always 0 (success)
  */
 
-int main() {
-	char charType;
-	int intType;
-	long int longintType;
-	long long int longlongintType;
-	float floatType;
-
-	printf("Size of a char: %d byte(s)\n", (sizeof(char));
-	printf("\nSize of an int: %d byte(s)\n", (sizeof(int));
-	printf("\nSize of a long int: %d bytes(s)\n", (sizeof(long int));
-	printf("\nSize of a long long int: %d bytes(s)\n", (sizeof(long long int));
-	printf("\nSize of a float: %d bytes(s)\n", (sizeof(float));
-	return 0;
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld bytes(s)\n", sizeof(float));
+	return (0);
 }
