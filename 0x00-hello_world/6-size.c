0af9c5d46cea88332b522ea96e2e78c03bb8e076
Mayen Kalu <kalumayen@gmail.com>
2022-09-08 18:53:16 -0700

    correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,3 @@
-/*  C program to print size of different data types using pointers  */
 
 #include <stdio.h>
 
@@ -6,12 +5,13 @@
  * main -prints the various strings with printf function
  * Return: Always 0 (success)
  */
-int main()
+
+int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", (sizeof(char));
 	printf("\nSize of an int: %d byte(s)\n", (sizeof(int));
 	printf("\nSize of a long int: %d bytes(s)\n", (sizeof(long int));
 	printf("\nSize of a long long int: %d bytes(s)\n", (sizeof(long long int));
-	printf("\nSize of a float: %d bytes(s)\n", (sizeof(float)):
-	return 0
+	printf("\nSize of a float: %d bytes(s)\n", (sizeof(float));
+	return (0);
 }
