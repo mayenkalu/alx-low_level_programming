cbe867058a769b83c4f1066577d86317a276757c
Mayen Kalu <kalumayen@gmail.com>
2022-09-08 20:41:32 -0700

    correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,13 +5,17 @@
  * Return: Always 0 (success)
  */
 
-int main(void)
+int main() {
+	char charType;
+	int intType;
+	long int longintType;
+	long long int longlongintType;
+	float floatType;
 
-{
 	printf("Size of a char: %d byte(s)\n", (sizeof(char));
 	printf("\nSize of an int: %d byte(s)\n", (sizeof(int));
 	printf("\nSize of a long int: %d bytes(s)\n", (sizeof(long int));
 	printf("\nSize of a long long int: %d bytes(s)\n", (sizeof(long long int));
 	printf("\nSize of a float: %d bytes(s)\n", (sizeof(float));
-	return (0);
+	return 0;
 }
