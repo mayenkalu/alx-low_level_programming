04df3518b32d94966447a698c542cc8985360e41
Mayen Kalu <kalumayen@gmail.com>
2022-09-10 15:35:36 -0700

    added task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print single digit numbers of bases 10,
+ * starting from 0
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for  (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
