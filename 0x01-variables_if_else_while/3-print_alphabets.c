ac394fde08adf0977e12371c7616981489579178
Mayen Kalu <kalumayen@gmail.com>
2022-09-09 21:44:03 -0700

    added task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 /**
  * main - prints the alphabet in lowercase,
  * using putchar function
@@ -16,7 +17,7 @@ int main(void)
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(toupper(ch));
-	}	
+	i}	
 		putchar('\n');
 		return (0);
 }
