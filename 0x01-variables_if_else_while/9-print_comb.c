a90acb5b1e445e1a5892c318fd5bb1e774380172
Mayen Kalu <kalumayen@gmail.com>
2022-09-10 17:53:14 -0700

    added continue statement within the loop

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,10 +12,10 @@ int main(void)
 	{
 		for (n = 48; n < 58; n++)
 			putchar(n);
-			putchar(',');
-			putchar(' ');
 		if (n == 57)
 		continue;
+			putchar(',');
+			putchar(' ');
 	}
 	putchar('\n');
 	return (0);
