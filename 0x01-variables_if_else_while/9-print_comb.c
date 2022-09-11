e9e42be668a55392e44307f84234e38b93ffb982
Mayen Kalu <kalumayen@gmail.com>
2022-09-10 18:34:19 -0700

    correction

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,13 +11,14 @@ int main(void)
 
 	{
 		for (n = 0; n < 10; n++)
-			putchar(n + 0);
-		if (n < 9)
 		{
-			putchar(',');
-			putchar(' ');
+			putchar(n + 0);
+			if (n < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
+		putchar('\n');
+		return (0);
 	}
-	putchar('\n');
-	return (0);
-}
