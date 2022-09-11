c095f4bb7dd077ddd1b0ea680a5dcd9552d95819
Mayen Kalu <kalumayen@gmail.com>
2022-09-10 21:06:17 -0700

    added task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	/**
+	 * all possible combination of 2 2-digits number
+	 * both seperated by space, and coma after
+	 */
+	int a, b, c;
+	/*loop through highest possible of a=98,with 1 less than b*/
+	for (a = 0 ; a <= 98 ; a++)
+	{
+	/*loop through highest of b=99,starting with 1 higherthan a*/
+		for (b = a + 1 ; b <= 99 ; b++)
+		{
+	/*print all two digit value of both numbers*/
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+	/*continue to put coma & space until final combination*/
+			if (a == 98 && b == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
