92ccee76205a294e45f1f8924580838b6a1489ed
Mayen Kalu <kalumayen@gmail.com>
2022-09-10 20:13:02 -0700

    identation

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,23 +6,22 @@
 int main(void)
 {
 	int l, m, n;
+
+	for (l = 48; l <= 58; l++)
 	{
-		for (l = 48; l <= 58; l++)
+		for (m = 49; m <= 58; m++)
 		{
-			for (m = 49; m <= 58; m++)
+			for (n = 50; n <= 58; n++)
 			{
-				for (n = 50; n <= 58; n++)
+				if (n > m && m > l)
 				{
-					if (n > m && m > l)
-					{
-						putchar(l);
-						putchar(m);
-						putchar(n);
+					putchar(l);
+					putchar(m);
+					putchar(n);
 					if (l != 55 || m != 56)
 					{
 						putchar(',');
 						putchar(' ');
-					}
 					}
 				}
 			}
