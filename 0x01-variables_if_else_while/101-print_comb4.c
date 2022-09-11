91883a22b285b3613c1983dce3c50fc251fe535e
Mayen Kalu <kalumayen@gmail.com>
2022-09-10 20:06:30 -0700

    added task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,18 +7,18 @@ int main(void)
 {
 	int l, m, n;
 	{
-		for (l = 48; l <= 56; l++)
+		for (l = 48; l <= 58; l++)
 		{
-			for (m = 49; m <= 57; m++)
+			for (m = 49; m <= 58; m++)
 			{
 				for (n = 50; n <= 58; n++)
 				{
-					if (n > m > l)
+					if (n > m && m > l)
 					{
 						putchar(l);
 						putchar(m);
 						putchar(n);
-					if (l != 56 || m != 57 || n != 58)
+					if (l != 55 || m != 56)
 					{
 						putchar(',');
 						putchar(' ');
