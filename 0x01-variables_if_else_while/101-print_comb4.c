b686d04e6463c689bb406f1c4100f09fff4ef1af
Mayen Kalu <kalumayen@gmail.com>
2022-09-10 20:30:40 -0700

    correction

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,21 +9,17 @@ int main(void)
 
 	for (l = 48; l <= 58; l++)
 	{
-		for (m = 49; m <= 58; m++)
+		for (m = l + 1; m <= 58; m++)
 		{
-			for (n = 50; n <= 58; n++)
+			for (n = m + 1; n <= 58; n++)
 			{
-				if (n > m && m > l)
-				{
-					putchar(l);
-					putchar(m);
-					putchar(n);
-					if (l != 55 || m != 56)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
+				putchar(l);
+				putchar(m);
+				putchar(n);
+				if (l == 57 && m == 58 && n == 59)
+					continue;
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
