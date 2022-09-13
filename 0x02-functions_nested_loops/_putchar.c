1b2717103ad9525fc9e0729bae79e30c1ef1e6d9
Mayen Kalu <kalumayen@gmail.com>
2022-09-13 16:16:45 -0700

    fixing the error

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - print with _putchar function
+ * Return: Always 0 (success)
+ * */
+int _putchar(void)
+	int char = x;
+{
+	_putchar();
+}
+	_putchar('\n');
+	return(0);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 /* This is my header file for */
 /* prototype functions in subsequent tasks */
 /* and _putchar function */
+
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -13,17 +14,3 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
-
-
-
-
-
-
-
-
-
-int _putchar(ch)
-{
-return (0);
-}
