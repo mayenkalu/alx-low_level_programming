0b58fd3b0b39f875d9a20e11093679b874a8afa5
Mayen Kalu <kalumayen@gmail.com>
2022-09-14 05:34:11 -0700

    fixed coding style in task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,8 +5,8 @@
  * @n: The number of whichthe sign will be printed.
  *
  * Return: 1 if the number is greater than zero,
- * 	   0 if number is zero,
- * 	   -1 if number is less than zero.
+ *	 0 if number is zero,
+ *	 -1 if number is less than zero.
  */
 int print_sign(int n)
 {
