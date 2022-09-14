dd4ba68429bb02536f67de2d1a264472f78fefec
Mayen Kalu <kalumayen@gmail.com>
2022-09-14 12:37:11 -0700

    correction

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  * print_to_98 - Prints all natural numbers from n to 98,
- *               in order separated by a comma followed by a space.
+ * in order separated by a comma followed by a space.
  * @n: The number to begin.
  */
 void print_to_98(int n)
@@ -20,4 +20,4 @@ void print_to_98(int n)
 			printf("%d, ", n++);
 		printf("%d\n", n);
 	}
-}}
+}
