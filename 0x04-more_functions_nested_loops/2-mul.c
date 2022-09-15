f0647188c75de6e86202ffe7c440be3a35d9fcff
Mayen Kalu <kalumayen@gmail.com>
2022-09-15 10:47:16 -0700

    added task 2 and 3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ * @a: integer to muliply by b.
+ * @b: integer to multiply by a.
+ *
+ * Return: Return result of multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9,
+ * followed by a new line.
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+	return (0);
+}
