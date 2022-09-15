8dde0a99cb9194db9dae0e13614eba158c891b07
Mayen Kalu <kalumayen@gmail.com>
2022-09-15 09:34:45 -0700

    added task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ *
+ * Return: 1 if character is uppercase, 0 if otherwise.
+ */
+
+int _isupper(int c)
+{
+	c = 'A' && c <= 'Z';
+	return (1);
+}
+
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+Exploring deep into nested loops.
+
