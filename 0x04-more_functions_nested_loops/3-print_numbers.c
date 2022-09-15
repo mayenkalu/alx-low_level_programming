254a5b2213ea03e56cdcda0eb0dbc88ba455e78b
Mayen Kalu <kalumayen@gmail.com>
2022-09-15 13:37:03 -0700

    added quote to numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,13 +3,14 @@
 /**
  * print_numbers - prints numbers from 0 to 9,
  * followed by a new line.
+ * Return: numbers.
  */
 
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = '0'; i < '10'; i++)
 	{
 		_putchar(i + '0');
 	}
