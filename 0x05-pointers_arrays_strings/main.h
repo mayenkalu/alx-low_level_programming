bc466b2b2bb8ae1f8d0bf3868f26c819d7db5357
Mayen Kalu <kalumayen@gmail.com>
2022-09-19 16:24:34 -0700

    header file

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+/**
+ * This is the header files containing the prototype functions.
+ * For 0x05- project.
+ */
+#ifndef MAIN_H
+#define MAIN_H
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+
+#endif /*MAIN_H*/
