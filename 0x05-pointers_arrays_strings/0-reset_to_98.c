ca50134dd358ea0f6385a7b3768af893ccb4f266
Mayen Kalu <kalumayen@gmail.com>
2022-09-19 16:28:16 -0700

    description to n as parameter in comment section

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - modifies the value a pointer points to to 98.
- * @*n: pointer as a parameter.
+ * @n: pointer as a parameter.
  * Return: Always 0.
  */
 void reset_to_98(int *n)
