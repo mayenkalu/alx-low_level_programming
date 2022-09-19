1271ece971fc990006d18c37993593006277aa5c
Mayen Kalu <kalumayen@gmail.com>
2022-09-19 16:50:14 -0700

    betty fix

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
  * _strlen - prints the length of a string.
- * @*s - string to check its length.
+ * @s: string to check its length.
  * Return: Always 0.
  */
 int _strlen(char *s)
 {
 	int c;
-	
+
 	c = 0;
 
 	while (s[c] != '\0')
-	{ 
+	{
 		c++;
 	}
 	return (c);
