64db3a461d20f30adfb0a4eb5a2cd879cbb6b2da
Mayen Kalu <kalumayen@gmail.com>
2022-09-19 21:26:39 -0700

    added task 8 through 11 to 0x05- project

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -13,4 +13,8 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 int _putchar(char c);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
 #endif /*MAIN_H*/
