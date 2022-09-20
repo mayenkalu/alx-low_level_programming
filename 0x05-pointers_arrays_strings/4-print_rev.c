0afb4835e3a16dedd621acfb018f953d468bb085
Mayen Kalu <kalumayen@gmail.com>
2022-09-19 21:42:29 -0700

    'betty-fixed task 4'
    	../0x04-more_functions_nested_loops/5-main.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,21 +6,17 @@
  */
 void print_rev(char *s)
 {
-        int a, b, len;
+	int a, b, len;
 
-        a = 0;
-
-        while (s[a] != '\0')
-        {
-                a++;
-        }
-
-        len = a;
-
-        for (b = len - 1; b >= 0; b--)
-        {
-                _putchar(s[b]);
-        }
-
-        _putchar('\n');
+	a = 0;
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	len = a;
+	for (b = len - 1; b >= 0; b--)
+	{
+		_putchar(s[b]);
+	}
+	_putchar('\n');
 }
