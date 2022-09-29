3abdd0475df3252183b046d7e24b9de318dca1e4
Mayen Kalu <kalumayen@gmail.com>
2022-09-29 04:04:24 -0700

    added task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * if_prime - checks if a given number is a prime number.
+ * @f: number to check.
+ * @d: number to divide with.
+ * Return: 1 if number is a prime number otherwise 0.
+ */
+int if_prime(int d, int f)
+{
+	if (f < 2 || f % d == 0)
+		return (0);
+	else if (d > f / 2)
+		return (1);
+	else
+		return (if_prime(d + 1, f));
+}
+
+
+/**
+ * is_prime_number - checks and prints if number is prime number.
+ * @n: number to check
+ *
+ * Return: 1 if prime number, otherwise 0
+ */
+int is_prime_number(int n)
+{
+	if (n == 2)
+		return (1);
+	return (if_prime(2, n));
+}
