de306c6876ed2347003a490a3acd1049ef587a4a
Mayen Kalu <kalumayen@gmail.com>
2022-09-29 04:55:13 -0700

    fixed task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  * factorial - prints fatorial of a given number.
- * @n - given number.
+ * @n: given number.
  * Return: -1 if number is lower than 0;
- * 	else return fatorial of number.
+ * else return fatorial of number.
  */
 int factorial(int n)
 {
 	if (n == 0)
 		return (1);
-	if	
+	else if	(n > 0)
 		return (n * factorial(n - 1));
 	else
 		return (-1);
