cbe4fc2906dd0cd41721f081b100e8bd8956c455
Mayen Kalu <kalumayen@gmail.com>
2022-09-29 04:59:09 -0700

    fixed task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
  * @x: number to be raised by a power(base).
  * @y: power of a number to raise(exponent).
  * Return: -1 if y is lower than 0;
- * 	else return value.
+ * else return value.
  */
 int _pow_recursion(int x, int y)
 {
