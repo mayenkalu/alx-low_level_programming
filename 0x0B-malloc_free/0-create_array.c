786102c14e90451f66e6e28a9a051cfad099d541
Mayen Kalu <kalumayen@gmail.com>
2022-10-04 17:55:24 -0700

    fixed syntax

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	a = malloc(sizeof(char) * size);
+	a = malloc(sizeof(c) * size);
 
 	if (a == NULL)
 		return (NULL);
